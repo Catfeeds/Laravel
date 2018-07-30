181b7f8cfff13a2ba0ca9f770f527b1589d203c8
zhukaihao <zhukaihaorj@163.com>
2018-07-30 09:48:56 +0800

    刷新/删除token

---
+++ app/Http/Controllers/AuthorizationController.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * User: ZhuKaihao
+ * Date: 2018/7/30
+ * Time: 上午9:28
+ */
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AuthorizationRequest;
+
+class AuthorizationController extends Controller
+{
+    public function store(AuthorizationRequest $request)
+    {
+        // TODO 邮箱也能登录 见教程4.5
+//        filter_var($username, FILTER_VALIDATE_EMAIL) ?
+//            $credentials['email'] = $username :
+//            $credentials['phone'] = $username;
+
+        $credentials['phone'] = $request->phone;
+        $credentials['password'] = $request->password;
+        if (!$token = \Auth::guard('api')->attempt($credentials)) {
+            return $this->response->errorUnauthorized('用户名或密码错误');
+        }
+        return $this->respondWithToken($token)->setStatusCode(201);
+    }
+
+    public function update()
+    {
+        $token = \Auth::guard('api')->refresh();
+        return $this->respondWithToken($token);
+    }
+
+    public function destroy()
+    {
+        \Auth::guard('api')->logout();
+        return $this->response->noContent();
+    }
+
+    protected function respondWithToken($token)
+    {
+        return $this->response->array([
+            'access_token' => $token,
+            'token_type'   => 'Bearer',
+            'expires_in'   => \Auth::guard('api')->factory()->getTTL() * 60
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,17 @@ $api->version('v1', ['namespace' => 'App\Http\Controllers'], function ($api) {
         ->name('verificationCode.store');
     // 用户注册
     $api->post('users', 'UserController@store')
-        ->name('user.store');
+        ->name('users.store');
+
+    // 登录
+    $api->post('authorizations', 'AuthorizationController@store')
+        ->name('authorizations.store');
+    // 刷新token
+    $api->put('authorizations/current', 'AuthorizationController@update')
+        ->name('authorizations.update');
+    // 删除token
+    $api->delete('authorizations/current', 'AuthorizationController@destroy')
+        ->name('authorizations.destroy');
 
     // 需要 token 验证的接口
     $api->group(['middleware' => 'api.auth'], function($api) {
