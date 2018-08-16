38fc2ff61f002f68f3da348319b5e12a2e4514f6
zhukaihao <zhukaihaorj@163.com>
2018-08-16 13:50:12 +0800

    自己的消息不算未读

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -2,8 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 class Thread extends \Cmgmyr\Messenger\Models\Thread
 {
+    // 所有未读消息
+    public function userUnreadMessages($userId)
+    {
+        $messages = $this->messages()->get();
+
+        try {
+            $participant = $this->getParticipantFromUser($userId);
+        } catch (ModelNotFoundException $e) {
+            return collect();
+        }
+
+        if (!$participant->last_read) {
+            return $messages;
+        }
+
+        return $messages->filter(function ($message) use ($participant) {
+            return $message->updated_at->gt($participant->last_read) &&
+                $message->user_id != $participant->id; // 添加这一行：自己发送的消息不算未读消息
+        });
+    }
 
     // 一次性设置所有的额外属性
     public function setExtraAttributes($user)
