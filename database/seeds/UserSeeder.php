71b9f68d4542947180ca17edb408b1e25d23b2a5
zhukaihao <zhukaihaorj@163.com>
2018-08-18 23:59:50 +0800

    评价通知

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -6,26 +6,29 @@ class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
      * @return void
      */
     public function run()
     {
-        \App\Models\User::insert( [
-            'name' => 'Zhu',
-            'phone' => '15650753237',
-            'type' => 'party',
-            'password' => bcrypt('123123'),
-            'created_at' => now(),
-            'updated_at' => now()
+        \App\Models\User::insert([
+            'name'            => 'Zhu',
+            'phone'           => '15650753237',
+            'type'            => 'party',
+            'email'           => 'zhukaihaorj@163.com',
+            'email_activated' => true,
+            'password'        => bcrypt('123123'),
+            'created_at'      => now(),
+            'updated_at'      => now()
         ]);
-        \App\Models\User::insert( [
-            'name' => 'Zhu222',
-            'phone' => '15650753236',
-            'type' => 'designer',
-            'password' => bcrypt('123123'),
-            'created_at' => now(),
-            'updated_at' => now()
+        \App\Models\User::insert([
+            'name'            => 'Zhu222',
+            'phone'           => '15650753236',
+            'type'            => 'designer',
+            'email'           => 'm15650753237@163.com',
+            'email_activated' => true,
+            'password'        => bcrypt('123123'),
+            'created_at'      => now(),
+            'updated_at'      => now()
         ]);
         factory(App\Models\User::class, 20)->create();
     }
