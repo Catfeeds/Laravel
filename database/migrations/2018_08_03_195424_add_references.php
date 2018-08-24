6549bac52b7ccfca731d2de2e37d53add89f85ca
zhukaihao <zhukaihaorj@163.com>
2018-08-24 16:04:54 +0800

    外键约束

--- database/migrations/2018_08_03_195424_add_references.php
+++ database/migrations/2018_08_03_195424_add_references.php
@@ -29,6 +29,38 @@ class AddReferences extends Migration
             // 当 topic_id 对应的 topics 表数据被删除时，删除此条数据
             $table->foreign('activity_id')->references('id')->on('activities')->onDelete('cascade');
         });
+        Schema::table('follow', function (Blueprint $table) {
+            // 当 user_id 对应的 users 表数据被删除时，删除此条数据
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('follower_id')->references('id')->on('users')->onDelete('cascade');
+        });
+        Schema::table('reviews', function (Blueprint $table) {
+            // 当 user_id 对应的 users 表数据被删除时，删除此条数据
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+        Schema::table('projects', function (Blueprint $table) {
+            // 当 user_id 对应的 users 表数据被删除时，删除此条数据
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+        Schema::table('project_favorites', function (Blueprint $table) {
+            // 当 user_id 对应的 users 表数据被删除时，删除此条数据
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+        });
+        Schema::table('project_applications', function (Blueprint $table) {
+            // 当 user_id 对应的 users 表数据被删除时，删除此条数据
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+        });
+        Schema::table('works', function (Blueprint $table) {
+            // 当 user_id 对应的 users 表数据被删除时，删除此条数据
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+        Schema::table('invitations', function (Blueprint $table) {
+            // 当 user_id 对应的 users 表数据被删除时，删除此条数据
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('invited_user_id')->references('id')->on('projects')->onDelete('cascade');
+        });
     }
 
     /**
@@ -49,6 +81,31 @@ class AddReferences extends Migration
         Schema::table('activity_likes', function (Blueprint $table) {
             $table->dropForeign(['user_id']);
             $table->dropForeign(['activity_id']);
+        });
+        Schema::table('follow', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['follower_id']);
+        });
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+        Schema::table('project_favorites', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['project_id']);
+        });
+        Schema::table('project_applications', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['project_id']);
+        });
+        Schema::table('works', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+        Schema::table('invitations', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['invited_user_id']);
         });
     }
 }
